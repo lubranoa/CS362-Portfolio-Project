f03cf03708bef94cc02903e6676925680f8673f3
Noah Calhoun <w.noah.calhoun@gmail.com>
2023-03-06 15:48:19 -0600

    implemented convert of base10 str

--- task.py
+++ task.py
@@ -1,4 +1,77 @@
+# A program by Noah Calhoun, Alex Lubrano, and Dacin Titus
+#
+#
 
-def my_func():
 
-    return "Hello World"
+def conv_num(num_str):
+    """This function takes in a string and converts it into a base 10 number, and returns it."""
+    periodFound = 0
+    hexValue = False
+    intValue = False
+    symbols = [".", "-"]
+    numbers = [0,1,2,3,4,5,6,7,8,9]
+    negFound = 0
+    deciFound = 0
+    deciLoc = 0
+    deciLocation = None
+
+    if num_str[0] == "0" and len(num_str) > 1 and num_str[1] == "x":
+        hexValue = True
+    else:
+        intValue = True
+
+    for i in range(len(num_str)):
+        # Handle base10 num
+        if intValue:
+            num = num_str[i]
+            if periodFound > 1:                             # check if multiple . found
+                return None
+            if num == ".":
+                periodFound += 1
+            if not num.isnumeric() and num not in symbols:  # Check if not a number nor symbol
+                return None
+
+            if num == "-":
+                if negFound > 1:
+                    return None
+                else:
+                    negFound += 1
+
+        # Handle hex num
+        elif hexValue:
+            return num_str
+
+        else:
+            return None
+        
+    # convert string to number for base10 string
+    if intValue:
+        result = 0
+        for char in num_str:
+            if char == '.':
+                deciFound = True
+                # continue
+            elif char == '-':
+                continue
+            else:
+                digit = ord(char) - ord('0')
+                if deciFound:
+                    deciLoc += 1
+                    result += digit / (10 ** deciLoc)
+                else:
+                    result = result * 10 + digit
+        if negFound:
+            result *= -1
+
+        return result
+    
+    if hexValue:
+        return num_str
+
+
+
+
+
+if __name__ == '__main__':
+    print(conv_num('-12.345'))
+    # print(conv_num('0x'))
--- tests.py
+++ tests.py
@@ -1,11 +1,28 @@
 import unittest
-
+from task import conv_num
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    # Begin conv_num tests
+    def test1_conv(self):
+        number = '12345'
+        self.assertEqual(conv_num(number), 12345)
+    
+    def test2_conv(self):
+        number = '-12345'
+        self.assertEqual(conv_num(number), -12345)
+        
+    def test3_conv(self):
+        number = '12.345'
+        self.assertEqual(conv_num(number), 12.345)
 
+    def test4_conv(self):
+        number = '-12.345'
+        self.assertEqual(conv_num(number), -12.345)
+
+    def test5_conv(self):
+        number = '0.45'
+        self.assertEqual(conv_num(number), 0.45)
 
 if __name__ == '__main__':
     unittest.main()
